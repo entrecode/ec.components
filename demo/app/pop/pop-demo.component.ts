6d06f204da6c15489141d8bab3313bd312488a69
Felix Roos <roos@entrecode.de>
2017-09-12 13:31:10 +0200

    updated pop demo

--- demo/app/pop/pop-demo.component.ts
+++ demo/app/pop/pop-demo.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { PopComponent } from '@ec.components/ui/src/pop/pop.component';
 
 @Component({
   selector: 'ec-pop-demo',
   templateUrl: './pop-demo.component.html',
 })
 export class PopDemoComponent {
+  private popClass: string;
+  @ViewChild('pop') pop: PopComponent;
+
   constructor() {
+  }
+
+  open(popClass: string = 'fullscreen') {
+    this.popClass = popClass;
+    this.pop.show();
   }
 }
