78779529187eca91975b5e671bd301159d781b20
Felix Roos <roos@entrecode.de>
2017-09-12 16:41:42 +0200

    cleaned up pop demo

--- demo/app/pop/pop-demo.component.ts
+++ demo/app/pop/pop-demo.component.ts
@@ -7,13 +7,14 @@ import { PopComponent } from '@ec.components/ui/src/pop/pop.component';
 })
 export class PopDemoComponent {
   private popClass: string;
+  private markup = require('./pop.markup.html');
   @ViewChild('pop') pop: PopComponent;
 
   constructor() {
   }
 
   open(popClass: string = 'fullscreen') {
-    this.popClass = popClass;
+    this.popClass = 'ec-pop_' + popClass;
     this.pop.show();
   }
 }
