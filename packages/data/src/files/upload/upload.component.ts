6285a9ecd32d74947525a010580f8a1e571e51d4
Felix Roos <roos@entrecode.de>
2017-09-27 10:42:11 +0200

    removed Hochladen which had to function + added possibility to trigger upload from outside

--- packages/data/src/files/upload/upload.component.ts
+++ packages/data/src/files/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ElementRef, ViewChild } from '@angular/core';
 import { SdkService } from '../../sdk/sdk.service';
 import { FileService, Upload } from '../file.service';
 import { LoaderComponent } from '@ec.components/ui/src/loader/loader.component';
@@ -8,7 +8,7 @@ import { NotificationsService } from '@ec.components/ui/src/notifications/notifi
 /** This component will render an input field to upload files to the datamanager. */
 @Component({
   selector: 'ec-upload',
-  template: `<input type="file" (change)="upload($event)" multiple>`,
+  template: `<input type="file" (change)="upload($event)" multiple #fileInput>`,
 })
 export class UploadComponent {
   /** The input placeholder*/
@@ -17,29 +17,39 @@ export class UploadComponent {
   @Input() loader: LoaderComponent;
   /** Emits when an upload is complete. */
   @Output() success: EventEmitter<Upload> = new EventEmitter();
+  /** Reference to the input[type=file] element */
+  @ViewChild('fileInput') fileInput: ElementRef;
 
   constructor(private sdk: SdkService,
     private fileService: FileService,
     private loaderService: LoaderService,
     private notificationService: NotificationsService) {
   }
+  /** opens the system upload window by triggering the input */
+  trigger() {
+    if (!this.fileInput) {
+      console.error('cannot trigger upload: file input element not found!');
+      return;
+    }
+    this.fileInput.nativeElement.click();
+  }
 
   /** Uploads the files from the input event. Handles loader and notifications. */
   upload(e) {
     const upload = this.fileService.uploadFiles(e)
-    .then((upload) => {
-      this.success.emit(upload);
-      this.notificationService.emit({
-        title: 'Upload erfolgreich',
-        type: 'success'
+      .then((_upload) => {
+        this.success.emit(_upload);
+        this.notificationService.emit({
+          title: 'Upload erfolgreich',
+          type: 'success'
+        });
+      }).catch((err) => {
+        console.log('error', err);
+        this.notificationService.emit({
+          title: 'Fehler beim Upload',
+          error: err
+        });
       });
-    }).catch((err) => {
-      console.log('error', err);
-      this.notificationService.emit({
-        title: 'Fehler beim Upload',
-        error: err
-      });
-    });
     this.loaderService.wait(upload, this.loader);
     return upload;
   }
