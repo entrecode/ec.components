30612f9743256e59269b09765fcb3c6f5d796222
Felix Roos <roos@entrecode.de>
2018-03-16 16:23:28 +0100

    simplified column hiding

--- packages/core/src/list/list.ts
+++ packages/core/src/list/list.ts
@@ -45,6 +45,7 @@ export class List<T> extends Collection<Item<T>> {
     }
     this.config = Object.assign({ page: 1, maxColumns: 8 }, config || {});
     this.fields = this.getFields();
+    this.hideOverflowFields();
     this.pagination = pagination || new Pagination(this.config, this.items.length);
     this.pagination.change$.debounceTime(200)
       .subscribe(_config => this.load(_config));
@@ -87,6 +88,17 @@ export class List<T> extends Collection<Item<T>> {
       });
     });
     return fields;
+  }
+
+  /** Sets all fields that exceed the maxColumns to hidden */
+  protected hideOverflowFields() {
+    if (this.config && this.config.maxColumns) {
+      this.fields.filter(f => !f.hidden).forEach((field, index) => {
+        if (index >= this.config.maxColumns) {
+          field.hidden = true;
+        }
+      });
+    }
   }
 
   /**
