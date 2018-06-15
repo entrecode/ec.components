baf6b9c1980d63a576c5c5d4ee2efb497dc6ec97
Felix Roos <roos@entrecode.de>
2018-06-15 14:28:42 +0200

    EDITOR-175 safe delete

--- packages/data/src/resource-delete-pop/resource-delete-pop.component.ts
+++ packages/data/src/resource-delete-pop/resource-delete-pop.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild, Output, EventEmitter, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { LoaderService, NotificationsService, PopComponent } from '@ec.components/ui';
 import { SymbolService } from '@ec.components/ui/src/symbol/symbol.service';
-import { PopComponent, LoaderService, NotificationsService } from '@ec.components/ui';
 import Resource from 'ec.sdk/lib/resources/Resource';
 import { ResourceService } from '../resource-config/resource.service';
 /** This component can be used to delete all kinds of resources with a confirmation pop.
@@ -16,12 +16,16 @@ import { ResourceService } from '../resource-config/resource.service';
     templateUrl: './resource-delete-pop.component.html',
 })
 export class ResourceDeletePopComponent {
+    /* The current value of the confirmation input */
+    confirmInput = '';
     /** The pop inside the view. */
     @ViewChild(PopComponent) pop: PopComponent;
     /** The resource to delete */
     @Input() resource: Resource;
     /** The relation where it happened */
     @Input() relation: string;
+    /** If given, an input will be shown that expects the string to be entered before being able to press delete */
+    @Input() safetyWord: string;
     /** Output that is after the deletion was successful. */
     @Output() deleted: EventEmitter<any> = new EventEmitter();
     /** Injects SymbolService and LoaderService */
@@ -35,13 +39,17 @@ export class ResourceDeletePopComponent {
             console.error('cannot delete: no resource given!');
             return;
         }
+        if (!this.canDelete()) {
+            console.warn('not confirmed!');
+            return;
+        }
         const deletion = this.resourceService.del(this.relation, this.resource)
             .then(res => {
                 this.notificationService.emit({
                     title: this.symbol.resolve('success.delete'),
                     type: 'success',
                 });
-                this.pop.hide();
+                this.hide()
                 this.deleted.next(res);
             }).catch(error => {
                 this.notificationService.emit({
@@ -51,9 +59,18 @@ export class ResourceDeletePopComponent {
             });
         this.loader.wait(deletion);
     }
+
+    canDelete() {
+        return !this.safetyWord || this.safetyWord === this.confirmInput;
+    }
     /** The confirm method sets a given resource and shows the confirmation pop. */
     confirm(resource: Resource = this.resource) {
         this.resource = resource;
         this.pop.show();
+    }
+
+    hide() {
+        this.pop.hide();
+        this.confirmInput = '';
     }
 }
