4eb84ec11ace8c7921898e6c73dcbc90cd55d98c
Felix Roos <roos@entrecode.de>
2018-09-18 11:03:02 +0200

    simplify imports again

--- packages/tinymce/src/tinymce.module.ts
+++ packages/tinymce/src/tinymce.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { TinymceComponent } from './tinymce/tinymce.component';
-import { FilesModule } from '@ec.components/data/src/files/files.module';
-import { PopModule } from '@ec.components/ui/src/pop/pop.module';
-import { IconModule } from '@ec.components/ui/src/icon/icon.module';
+import { UiModule } from '@ec.components/ui';
+import { DataModule } from '@ec.components/data';
 
 @NgModule({
   declarations: [TinymceComponent],
   entryComponents: [TinymceComponent],
-  imports: [IconModule, PopModule, FilesModule],
-  exports: [TinymceComponent, IconModule, PopModule, FilesModule]
+  imports: [UiModule, DataModule],
+  exports: [TinymceComponent]
 })
 export class TinymceModule {
 }
