57933ba89f16d41f3b31680ca2bdad1994aa6c46
Felix Roos <roos@entrecode.de>
2018-09-18 10:53:30 +0200

    fixed files.module import

--- packages/tinymce/src/tinymce.module.ts
+++ packages/tinymce/src/tinymce.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { TinymceComponent } from './tinymce/tinymce.component';
-import { FilesModule } from '@ec.components/data/src/files';
+import { FilesModule } from '@ec.components/data/src/files/files.module';
 import { PopModule } from '@ec.components/ui/src/pop/pop.module';
 import { IconModule } from '@ec.components/ui/src/icon/icon.module';
 
