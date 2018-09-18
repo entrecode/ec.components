9b07dba671bd7963ce19414b20fbff283933d23e
Felix Roos <roos@entrecode.de>
2018-09-18 10:41:18 +0200

    import files module..

--- packages/tinymce/src/tinymce.module.ts
+++ packages/tinymce/src/tinymce.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { TinymceComponent } from './tinymce/tinymce.component';
 import { UiModule } from '@ec.components/ui';
+import { FilesModule } from '@ec.components/data/src/files';
 
 @NgModule({
   declarations: [TinymceComponent],
   entryComponents: [TinymceComponent],
-  imports: [UiModule],
-  exports: [UiModule]
+  imports: [UiModule, FilesModule],
+  exports: [UiModule, FilesModule]
 })
 export class TinymceModule {
 }
