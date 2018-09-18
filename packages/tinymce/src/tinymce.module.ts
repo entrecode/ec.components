c46f75f36856e29de14760bd98f0264f1e893055
Felix Roos <roos@entrecode.de>
2018-09-18 10:45:12 +0200

    added ui+data to tinymce package

--- packages/tinymce/src/tinymce.module.ts
+++ packages/tinymce/src/tinymce.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { TinymceComponent } from './tinymce/tinymce.component';
-import { UiModule } from '@ec.components/ui';
 import { FilesModule } from '@ec.components/data/src/files';
+import { PopModule } from '@ec.components/ui/src/pop/pop.module';
+import { IconModule } from '@ec.components/ui/src/icon/icon.module';
 
 @NgModule({
   declarations: [TinymceComponent],
   entryComponents: [TinymceComponent],
-  imports: [UiModule, FilesModule],
-  exports: [UiModule, FilesModule]
+  imports: [IconModule, PopModule, FilesModule],
+  exports: [TinymceComponent, IconModule, PopModule, FilesModule]
 })
 export class TinymceModule {
 }
