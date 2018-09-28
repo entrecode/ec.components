e79ea0b691f8c9bde653ab15a7fe21842f81e20e
Felix <felix@Huxi-Puxi-Fluxi.fritz.box>
2018-09-28 14:40:58 +0200

    doc build + fixed hideMenu in demo

--- packages/ui/src/modal/modal.component.ts
+++ packages/ui/src/modal/modal.component.ts
@@ -2,12 +2,17 @@ import { Component, Input, OnInit, ElementRef, OnChanges } from '@angular/core';
 import { PopComponent } from '@ec.components/ui/src/pop/pop.component';
 import { PopService } from '@ec.components/ui/src/pop/pop.service';
 
+/**
+ * A modal is an extension of a pop. It adds different style options that apply x.ui markup.
+ * <example-url>https://components.entrecode.de/ui/modal?e=1</example-url>
+ */
 @Component({
     selector: 'ec-modal',
     templateUrl: './modal.component.html'
 })
 
 export class ModalComponent extends PopComponent implements OnInit, OnChanges {
+    /** all possible modes that can be set */
     types = [
         'dialog',
         'overlay',
@@ -15,15 +20,20 @@ export class ModalComponent extends PopComponent implements OnInit, OnChanges {
         'toast',
         'snackbar'
     ];
-    /** Set host class to make sure the type is used */
+    /** This property sets the mode + additional options.
+     * It expects the type at first and non mandatory options like opens-left afterwards. */
     @Input() mode: string;
+    /** If true, the modal will have a dark backdrop that disables clicking outside. */
     @Input() backdrop = false;
+    /** Sets data-col. If specified, the width of the modal is fixed to the given value (1-12). */
     @Input() columns: number | string;
-    classes: string;
+    /** Will contain the classes (mode - type) */
+    private classes: string;
+    /** Constructs the modal, injects pop service */
     constructor(public popService: PopService, public elementRef: ElementRef) {
         super(popService);
     }
-
+    /** Is called on init and change. Parses mode input and throws warning if type is not supported. */
     initMode() {
         this.mode = this.mode || 'dialog';
         this.type = this.mode.split(' ')[0];
@@ -43,10 +53,11 @@ export class ModalComponent extends PopComponent implements OnInit, OnChanges {
         }
     }
 
+    /** calls initMode */
     ngOnInit() {
         this.initMode();
     }
-
+    /** calls initMode */
     ngOnChanges() {
         this.initMode();
     }
