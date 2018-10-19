974a921fb0a607fbaa9cb76a5fbf52b769d705c1
Felix Roos <roos@entrecode.de>
2018-10-19 14:44:25 +0200

    EDITOR-231 now using new getFieldConfig method to generate model config

--- demo/app/entry-pop-demo/entry-pop-demo.component.ts
+++ demo/app/entry-pop-demo/entry-pop-demo.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { ModelConfigService } from '@ec.components/data';
 
 @Component({
   templateUrl: './entry-pop-demo.component.html',
 })
 export class EntryPopDemoComponent {
+  popConfig = {
+    fields: {
+      name: {
+        label: 'Name im POP!'
+      },
+      amazement_factor: {
+        columns: 6
+      },
+      test_asset: {
+        columns: 6
+      }
+    }
+  }
 
+  constructor(public modelConfig: ModelConfigService) {
+    this.modelConfig.set('muffin', {
+      fields: {
+        id: {
+          hidden: true
+        },
+        name: {
+          label: 'Name..'
+        },
+        amazement_factor: {
+          label: 'amazement_factorororororor'
+        },
+        test_asset: {
+          label: 'teststststst'
+        }
+      }
+    });
+  }
 }
