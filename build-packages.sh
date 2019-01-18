a1e3f79a2240b24789beb4983120be1f925749ff
Felix Roos <roos@entrecode.de>
2019-01-18 12:09:08 +0100

    optimized build-packages script

--- build-packages.sh
+++ build-packages.sh
@@ -1,8 +1,8 @@
-ng build core > packages/core/build-log.txt
-ng build calendar > packages/calendar/build-log.txt
-ng build ui > packages/ui/build-log.txt
-ng build location > packages/location/build-log.txt
-ng build data > packages/data/build-log.txt
-ng build ace > packages/ace/build-log.txt
-ng build medium-editor > packages/medium-editor/build-log.txt
-ng build tinymce > packages/tinymce/build-log.txt
+ng build core &&
+ng build calendar &&
+ng build ui &&
+ng build location &&
+ng build data &&
+ng build ace &&
+ng build medium-editor &&
+ng build tinymce > build-packages.log
