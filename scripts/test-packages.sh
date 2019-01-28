8c29c012c57519a5e1fba6c11bdb6d8a180058af
Felix Roos <roos@entrecode.de>
2019-01-28 12:05:40 +0100

    docs: build + fix: medium-editor test command

--- scripts/test-packages.sh
+++ scripts/test-packages.sh
@@ -4,5 +4,5 @@ sh scripts/test-package.sh ui &&
 sh scripts/test-package.sh location &&
 sh scripts/test-package.sh data &&
 sh scripts/test-package.sh ace &&
-sh scripts/test-package.sh medium &&
+sh scripts/test-package.sh medium-editor &&
 sh scripts/test-package.sh tinymce
