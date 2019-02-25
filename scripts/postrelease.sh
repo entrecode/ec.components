1ccc1506e9f031518b29963f815b992264036ded
Felix Roos <roos@entrecode.de>
2019-02-25 12:05:32 +0100

    chore: postrelease commit msg

--- scripts/postrelease.sh
+++ scripts/postrelease.sh
@@ -5,7 +5,7 @@ git diff-index --quiet HEAD --
 git checkout develop
 npm run docs
 git add -A
-git commit --no-verify  -m "docs: build docs postrelease"
+git commit --no-verify  -m "docs: build docs #postrelease"
 git checkout master
 git merge --no-ff --no-verify  -m "chore: merge develop into master #postrelease" develop
 git push --no-verify
