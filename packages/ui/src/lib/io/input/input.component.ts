35022a6e07a36af342eca4cef38fb5a1a631915f
Felix Roos <roos@entrecode.de>
2019-03-29 09:36:00 +0100

    fix: improved input focus behaviour

--- packages/ui/src/lib/io/input/input.component.ts
+++ packages/ui/src/lib/io/input/input.component.ts
@@ -86,7 +86,8 @@ export class InputComponent extends DynamicSlotComponent implements ControlValue
       item: this.item,
       field: this.field,
       input: this,
-      config: this.config || this.field.config || {}
+      config: this.config || this.field.config || {},
+      focusEvent: this.focusEvent
     };
     const componentRef = this.loadComponent(this.component || this.field.input || DefaultInputComponent, data);
     this.componentInstance = componentRef.instance;
