65fa5581fdca3620d806eb042944a8131547afa1
Felix Roos <roos@entrecode.de>
2019-04-26 10:05:18 +0200

    fix: location picker spec

--- packages/location/src/lib/location.picker.component.spec.ts
+++ packages/location/src/lib/location.picker.component.spec.ts
@@ -1,23 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LocationPickerComponent } from './location-picker.component';
 import { locationModuleConfig } from './location.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LocationPickerComponent', () => {
-    let component: LocationPickerComponent;
-    let fixture: ComponentFixture<LocationPickerComponent>;
+  let component: LocationPickerComponent;
+  let fixture: ComponentFixture<LocationPickerComponent>;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule(locationModuleConfig)
-            .compileComponents();
-    }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      ...locationModuleConfig,
+      imports: [
+        ...locationModuleConfig.imports,
+        RouterTestingModule
+      ]
+    })
+      .compileComponents();
+  }));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(LocationPickerComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LocationPickerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
